215d4be5b72d7a22a2997128d19c909e73bb4351
YazanJbara <yazanjbara20034456@gmail.com>
2025-07-24 16:46:44 +0300

    dynamically displaying the random option

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-// global variables
+// global variables & DOM selections
 const options = ['rock', 'paper', 'scissors'];
 let humanScore = 0;
 let computerScore = 0;
+
+
+const humanPoints = document.querySelector('.user-score');
+const computerPoints = document.querySelector('.computer-score');
+
+const playBtn = document.querySelector('.btn--play');
+const resetBtn = document.querySelector('.btn--reset');
+
+const gesture = document.getElementById('computer-gesture');
+
+
+
+// computer random logic
+playBtn.addEventListener('click', function () {
+ const randomGesture = Math.trunc(Math.random() * 3) + 1;
+  gesture.src = `Assets/gesture--${randomGesture}.svg`;
+});
 
 
 // generate computer choice randomly
@@ -14,22 +31,21 @@ function getComputerChoice() {
 
 // taking user input
 
-function getPlayerChoice () {
-  let validatedinput = false;
-  while (validatedinput==false) {
-    const choice = prompt("enter rock || paper || scissors")
-    if (choice == null) {
-      continue;
-    }
-    const inputInLowerCase = choice.toLowerCase()
-    if (options.includes(inputInLowerCase)) {
-      validatedinput = true
-      return inputInLowerCase;
-    }
-  }
+// function getPlayerChoice () {
+//   let validatedinput = false;
+//   while (validatedinput==false) {
+//     const choice = prompt("enter rock || paper || scissors")
+//     if (choice == null) {
+//       continue;
+//     }
+//     const inputInLowerCase = choice.toLowerCase()
+//     if (options.includes(inputInLowerCase)) {
+//       validatedinput = true
+//       return inputInLowerCase;
+//     }
+//   }
 
-}
-
+// }
 
 // function to check who won
 
@@ -44,23 +60,19 @@ function playRound(humanChoice, computerChoice) {
     return 'Tie';
   } else {
     return `computer won cz he choosed ${computerChoice} vs human choosed ${humanChoice} `;
-    
   }
 }
 
-
-
 // the entire game
 
-function playGame () {
-  for (let i=0 ; i<5 ; i++) {
+function playGame() {
+  for (let i = 0; i < 5; i++) {
     const humanSelection = getPlayerChoice();
 
     const computerSelection = getComputerChoice();
 
     console.log(playRound(humanSelection, computerSelection));
   }
-
 }
 
-playGame ()
+playGame();
